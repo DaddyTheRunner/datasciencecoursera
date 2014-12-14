68ca361a0a279d728050eb7ed26dfd9bc3e78f5b
Daddy The Runner <DaddyTheRunner@users.noreply.github.com>
2014-12-14 15:18:47 -0500

    Updated run_analysis.R
    - Checks to make sure we are in the correct working directory
    - Now it reads in all of the data

--- get-clean-data-proj/run_analysis.R
+++ get-clean-data-proj/run_analysis.R
@@ -9,6 +9,14 @@
 #
 ###################
 
+## Load the required libraries
+require(stringi)
+
+## Check to see if we are in the correct working directory
+if (!stri_endswith_fixed(getwd(), "get-clean-data-proj")) {
+  stop("Not runing in the get-clean-data-proj directory!")
+}
+
 ## Create a local directory for the data if it doesn't already exist
 data.dir <- "./Data"
 if (!file.exists(data.dir)) { dir.create(data.dir) }
@@ -25,3 +33,27 @@ if (!file.exists(file.path(data.dir, data.file))) {
 if (!file.exists(file.path(data.dir, "UCI HAR Dataset"))) {
   unzip(file.path(data.dir, data.file), exdir = data.dir)
 }
+
+## Set some file names for reading in the data files
+## First update the data directory
+data.dir <- "./Data/UCI HAR Dataset/"
+features.fn <- "features.txt"
+xTrain.fn <- "train/X_train.txt"
+xTest.fn <- "test/X_test.txt"
+yTrain.fn <- "train/y_train.txt"
+yTest.fn <- "test/y_test.txt"
+subjTrain.fn <- "train/subject_train.txt"
+subjTest.fn <- "test/subject_test.txt"
+actLbls.fn <- "activity_labels.txt"
+
+## Now read in the data
+features <- read.table(file.path(data.dir, features.fn), stringsAsFactors = FALSE)
+xTrain <- read.table(file.path(data.dir, xTrain.fn), stringsAsFactors = FALSE)
+yTrain <- read.table(file.path(data.dir, yTrain.fn), stringsAsFactors = FALSE)
+xTest <- read.table(file.path(data.dir, xTest.fn), stringsAsFactors = FALSE)
+yTest <- read.table(file.path(data.dir, yTest.fn), stringsAsFactors = FALSE)
+subjTrain <- read.table(file.path(data.dir, subjTrain.fn), stringsAsFactors = FALSE)
+subjTest <- read.table(file.path(data.dir, subjTest.fn), stringsAsFactors = FALSE)
+actLbls.fn <- read.table(file.path(data.dir, actLbls.fn), stringsAsFactors = FALSE)
+
+## Now we need to stitch the data together
